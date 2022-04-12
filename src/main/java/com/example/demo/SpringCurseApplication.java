432ddd30415af2b1c9da36f7e6e6039706dfa96c
Willian Bogler <willianbogler@hotmail.com>
2022-04-12 15:52:23 -0300

    iniciando projeto

---
+++ src/main/java/com/example/demo/SpringCurseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCurseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCurseApplication.class, args);
+	}
+
+}
