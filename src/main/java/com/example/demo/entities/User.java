ae7c774a0b542da3613fc1e32a3d905da7ec11ae
Willian Bogler <willianbogler@hotmail.com>
2022-04-12 20:40:17 -0300

    conclusão da criaçãos das tabelas e er para orders

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -22,6 +24,7 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "client")	
 	private List<Order> orders = new ArrayList<>();
 	
