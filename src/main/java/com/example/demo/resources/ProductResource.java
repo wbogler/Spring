8298ed3a90147e5cfb7691e874a8a66310514b7a
Willian Bogler <willianbogler@hotmail.com>
2022-04-18 13:28:22 -0300

    Adição dos produtos

---
+++ src/main/java/com/example/demo/resources/ProductResource.java
@@ -0,0 +1,33 @@
+package com.example.demo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Product;
+import com.example.demo.service.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductService userservice;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll(){
+		List<Product> list = userservice.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		Product obj = userservice.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
